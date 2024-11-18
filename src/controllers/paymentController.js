f078a23538af2fb8f95ad911d39ffa606dd11b58
mansanjim1 <mansanjim1@alum.us.es>
2024-11-18 19:35:11 +0100

    upload of the get payments functionality

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -32,4 +32,38 @@ export const registerPayment = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 }
+export const obtainAllPayments = async (req, res) => {
+  try {
+    const payments = await Payment.find(); // Encuentra todos los pacientes
+    res.status(200).json(payments);
+  } catch (error) {
+    logger.error('Error fetching payments', {
+      method: req.method,
+      url: req.originalUrl,
+      error: error
+    });
+    res.status(500).json({ message: error.message });
+  }
+}
+export const getPaymentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const payment = await Payment.findById(id); 
 
+    if (!payment) {
+      return res.status(404).json({ message: 'Payment not found' });
+    }
+
+    logger.info(`Payment ${payment._id} retrieved`);
+    res.status(200).json(payment);
+
+  } catch (error) {
+    logger.error('Error retrieving payment', {
+      method: req.method,
+      url: req.originalUrl,
+      error: error
+    });
+
+    res.status(500).json({ message: 'An error occurred while retrieving the payment' });
+  }
+};
